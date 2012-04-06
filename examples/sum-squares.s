41ce97a8b3deb0c69f950aa6a8d34ec85fa36a8d
Jasper Van der Jeugt <m@jaspervdj.be>
2012-04-06 20:59:11 +0200

    sum [x * x | x <- [0 .. 50]] in dcpu-16 assembly

---
+++ examples/sum-squares.s
@@ -0,0 +1,15 @@
+; X <- sum [n * n | n < [0 .. 50]]
+; When finished, X should be 42925 (0xa7ad)
+        SET X, 0   ; Sum accumulator
+        SET I, 0   ; Iterator value
+        SET Y, 50  ; Max value
+
+:loop   SET A, I
+        MUL A, A
+        ADD X, A
+        ADD I, 1
+        IFG I, Y
+        SET PC, done
+        SET PC, loop
+
+:done   SET I, I
